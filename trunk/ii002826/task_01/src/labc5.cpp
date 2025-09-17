6230616450c63f572af59c0b3476baa594bf591a
Tlbery <ii002826@g.bstu.by>
2025-09-17 15:06:02 +0300

    Auto stash before rebase of "Lab1"

--- trunk/ii002826/task_01/src/labc5.cpp
+++ trunk/ii002826/task_01/src/labc5.cpp
@@ -1,67 +1,53 @@
-#include <iostream>
-#include <cmath>
+Код программы:
+﻿#include <iostream>
 
 using namespace std;
 
-double f(double x, double y, double z, int c) {
-    return pow(x, 3) * pow(y, 2) * z - 2 * pow(x, 2) * y * z + 3 * x * y + c;
+double a = 0.5,
+b = 1,
+c = 0.15,
+d = 0.3,
+u = 1.2;
+
+
+void liner(double& y1)
+{
+	y1 = a * y1 + b * u;
+	cout << y1 << endl;
 }
 
-double dx(double x, double y, double z) {
-    return 3 * pow(x, 2) * pow(y, 2) * z - 4 * x * y * z + 3 * y;
+void unliner(double& y2, double& y_pr, bool& second_iteration)
+{
+	if (second_iteration)
+	{
+		y_pr = y2;
+		y2 = a * y2 + c * u + d * sin(u); 
+		cout << y2 << endl;
+		second_iteration = false;
+	}
+	else
+	{
+		double buf;
+		buf = a * y2 - b * pow(y_pr, 2) + c * u + d * sin(u);
+		y_pr = y2;
+		y2 = buf;
+		cout << y2 << endl;
+	}
 }
 
-double dy(double x, double y, double z) {
-    return 2 * pow(x, 3) * y * z - 2 * pow(x, 2) * z + 3 * x;
-}
 
-double dz(double x, double y, double z) {
-    return pow(x, 3) * pow(y, 2) - 2 * pow(x, 2) * y;
-}
-
-int main() {
-    int sn = 9;
-    int fn = 6;
-    int hn = 8;
-
-    int cc;
-    cout << "Введите c: ";
-    cin >> cc;
-
-    double a = 10, b = 7, c = 9;
-    int iteration = 10;
-    double adam = 0.000001;
-
-    for (int i = 0; i < iteration; i++) {
-        while (a >= (sn - 2) && a <= (sn + 2)) {
-            double a1 = a - adam * dx(a, b, c);
-            if (a1 < (sn - 2) || a1 > (sn + 2)) {
-                break;
-            } else {
-                a = a1;
-            }
-        }
-
-        while (b >= (fn - 2) && b <= (fn + 2)) {
-            double b1 = b - adam * dy(a, b, c);
-            if (b1 < (fn - 2) || b1 > (fn + 2)) {
-                break;
-            } else {
-                b = b1;
-            }
-        }
-
-        while (c >= (hn - 3) && c <= (hn + 3)) {
-            double c1 = c - adam * dz(a, b, c);
-            if (c1 < (hn - 3) || c1 > (hn + 3)) {
-                break;
-            } else {
-                c = c1;
-            }
-        }
-    }
-
-    cout << "Координаты локального минимума [" << a << ", " << b << ", " << c << "], значение функции " << f(a, b, c, cc) << endl;
-
-    return 0;
+int main()
+{
+	double y1, y2, y_pr;
+	bool second_iteration = true;
+	cout << "Input temperature: ";
+	cin >> y1;
+	y2 = y1;
+	int n = 10;
+	cout << "Liner model\n" << y1 << endl;
+	for (int i = 0; i < n; i++)
+		liner(y1);
+	cout << endl << "Unliner model\n" << y2 << endl;
+	for (int i = 0; i < n; i++)
+		unliner(y2, y_pr, second_iteration);
 }
