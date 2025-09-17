704122a58e7de88b8b148f3c899f95ca8eba2fee
Артёмка <temchikstepannovikov@gmail.com>
2025-09-17 14:43:00 +0300

    adding .cpp file

---
+++ trunk/ii02823/task_01/src/task_01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    double y, u, a, b, c, d;
+    cout << "Enter y and u values:\n";
+    cin >> y >> u;
+    cout << "Enter a, b, c, d values:\n";
+    cin >> a >> b >> c >> d;
+
+    int n;
+    cout << "Enter number of steps n: ";
+    cin >> n;
+
+    double y1 = y, y2 = y;
+    for (int i = 0; i < n; i++)
+    {
+        y1 = a * y1 + b * u;
+        y2 = a * y2 - b * y2 * y2 + c * u + d * sin(u);
+        cout << "Result of the " << i + 1 << "st step of linear model: " << y1 << ";\n";
+        cout << "Result of the " << i + 1 << "st step of non-linear model: " << y2 << ";\n";
+    }
+
+    return 0;
+}
