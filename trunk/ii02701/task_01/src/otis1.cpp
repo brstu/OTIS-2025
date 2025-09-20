268a600ac3153a55b8124cfb6e8aa33060fd1431
Nikita <ii002701@g.bstu.by>
2025-09-20 14:33:42 +0300

    Add src folder with .cpp and CMake files

---
+++ trunk/ii02701/task_01/src/otis1.cpp
@@ -0,0 +1,37 @@
+﻿#include  <iostream>
+#include <cmath>
+
+int  main()
+{
+	double y;
+	double y_prev;
+	double y_next;
+	double u;
+	double a;
+	double b;
+	double c;
+	double d;
+	int n;
+	std::cout << "Enter y(starting temperature) and u(input warm at every step) values: ";
+	std::cin >> y >> u;
+	std::cout << "Enter a, b, c, d(constants) values: ";
+	std::cin >> a >> b >> c >> d;
+	std::cout << "Enter number of steps n: ";
+	std::cin >> n;
+	y_prev = y;
+	for (int i = 0; i < n; i++)
+	{
+		y = a * y + b * u;
+		std::cout << "Result of the " << i + 1 << " step of linear model: " << y << '\n';
+	}
+	y = y_prev;
+	y_prev = 0;
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << "Result of the " << i + 1 << " step of non-linear model: " << y << '\n';
+		y_next = a * y - b * y_prev * y_prev + c * u + d * sin(u);
+		y_prev = y;
+		y = y_next;
+	}
+	return 0;
+}
