3f08c9a5753cb933662133348749e4bb611b1a7c
ChuikaM <matveidoroshenko@gmail.com>
2025-09-23 11:54:18 +0300

    feat: update main.cpp: add input validation, change type of u to int in simulate method, add to class's attributes const keyword.

--- trunk/ii02707/task_01/src/main.cpp
+++ trunk/ii02707/task_01/src/main.cpp
@@ -5,21 +5,21 @@
 class ISimulatedModel
 {
 public:
-    virtual void simulate(double y, const double u, double t) const = 0;
+    virtual void simulate(double y, const double u, int t) const = 0;
     virtual ~ISimulatedModel() = default;
 
 };
 class LinearModel : public ISimulatedModel
 {
 public:
-    LinearModel(double a, double b)
+    LinearModel(const double a, const double b)
         : m_a(a), m_b(b) 
     {}
     ~LinearModel() override = default;
 
-    void simulate(double y, const double u, double t) const override
+    void simulate(double y, const double u, int t) const override
     {   
-        for(int i = 0; i <= static_cast<int>(t); i++)
+        for(int i = 0; i <= t; i++)
         {
             std::cout << i << ' ' << y << std::endl;
             y = m_a * y + m_b * u;
@@ -34,15 +34,15 @@ private:
 class NonLinearModel : public ISimulatedModel
 {
 public:
-    NonLinearModel(double a, double b, double c, double d)
+    NonLinearModel(const double a, const double b, const double c, const double d)
             : m_a(a), m_b(b), m_c(c), m_d(d) 
     {}
     ~NonLinearModel() override = default;
 
-    void simulate(double y, const double u, double t) const override
+    void simulate(double y, const double u, int t) const override
     {
-        double prevY = 0;
-        for(int i = 0; i <= static_cast<int>(t); i++)
+        double prevY = y;
+        for(int i = 0; i <= t; i++)
         {
             std::cout << i << ' ' << y << std::endl;
             double nextY = m_a * y - m_b * prevY * prevY + m_c * u + m_d * sin(u);
@@ -76,8 +76,8 @@ public:
     { return std::make_unique<LinearModel>(m_a, m_b); }
 
 private:
-    double m_a { 0.5 };
-    double m_b { 0.5 };
+    const double m_a { 0.5 };
+    const double m_b { 0.5 };
 
 };
 class FactoryNonLinearModel : public IFactoryModel
@@ -89,12 +89,21 @@ public:
     { return std::make_unique<NonLinearModel>(m_a, m_b, m_c, m_d); }
 
 private:
-    double m_a { 0.5 };
-    double m_b { 0.5 };
-    double m_c { 0.5 };
-    double m_d { 0.5 };
+    const double m_a { 0.5 };
+    const double m_b { 0.5 };
+    const double m_c { 0.5 };
+    const double m_d { 0.5 };
 
 };
+
+template<typename T>
+void validateInput(const std::string &txt, T& value)
+{
+	do
+	{
+		std::cout << "y:"; std::cin >> value;
+	} while(value < 0);
+}
 
 int main() 
 {
@@ -103,11 +112,11 @@ int main()
     
     std::cout << "Write necessary data for calculation:" << std::endl;
     double y;
-    std::cout << "y:"; std::cin >> y;
+	validateInput("y:", y);
     double u;
-    std::cout << "u:"; std::cin >> u;
-    double t;
-    std::cout << "t:"; std::cin >> t;
+	validateInput("u:", u);
+    int t;
+	validateInput("t:", t);
     std::cout << std::endl;
 
     std::cout << "Linear simulation:" << std::endl;
