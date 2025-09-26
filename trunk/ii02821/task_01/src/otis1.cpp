591445e3abc52541c41c42ca6395d40bfc60e7df
humoristoff <bmw.dimon1994@gmail.com>
2025-09-26 11:10:08 +0300

    Changed otis1.cpp

--- trunk/ii02821/task_01/src/otis1.cpp
+++ trunk/ii02821/task_01/src/otis1.cpp
@@ -17,9 +17,6 @@ void simulateLinear(double a, double b, double u, int steps) {
 }
 
 double nextNonlinear(double a, double b, double c, double d, double u, double y, double yPrev) {
-    if (yPrev == 0) {
-        return a * y - b * (yPrev * yPrev) + c * u + d * sin(u);
-    }
     return a * y - b * (yPrev * yPrev) + c * u + d * sin(u);
 }
 
