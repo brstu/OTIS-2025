c42b64e58033a3832d9629c104bde3639550db49
VL <sabakailili087@gmail.com>
2025-09-27 15:23:53 +0300

    Fix bug on lab.cpp

--- trunk/ii02818/task_01/src/lab1OTIS.cpp
+++ trunk/ii02818/task_01/src/lab1OTIS.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
 
 int main() {
-	std::cout << "Program started successfully!" << std::endl;
-	return 0;
+    int n;
+    double a, b, c, d, y0, u0;
+    cin >> n >> a >> b >> c >> d >> y0 >> u0;
+    vector<double> u(n + 1), y(n + 1);
+    u[0] = u0;
+    y[0] = y0;
+    for (int t = 0; t < n; t++) {
+        cin >> u[t + 1];
+        y[t + 1] = a * y[t] - b * y[t] * y[t] + c * u[t] + d * sin(u[t]);
+    }
+    for (int t = 0; t <= n; t++) cout << t << " " << y[t] << "\n";
 }
