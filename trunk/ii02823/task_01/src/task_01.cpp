4df88194809ef7e2242131e784b1eabdb9039f01
Артём Степанюк <46921624+tstepannovikov@users.noreply.github.com>
2025-10-01 09:59:36 +0300

    Deleted namespace std

--- trunk/ii02823/task_01/src/task_01.cpp
+++ trunk/ii02823/task_01/src/task_01.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
-
 int main()
 {
     double y, u;
-    cout << "Enter y and u values:\n";
-    cin >> y >> u;
+    std::cout << "Enter y and u values:\n";
+    std::cin >> y >> u;
     
     double a, b, c, d;
-    cout << "Enter a, b, c, d values:\n";
-    cin >> a >> b >> c >> d;
+    std::cout << "Enter a, b, c, d values:\n";
+    std::cin >> a >> b >> c >> d;
 
     int n;
-    cout << "Enter number of steps n: ";
-    cin >> n;
+    std::cout << "Enter number of steps n: ";
+    std::cin >> n;
 
     double y1 = y, y2 = y;
     for (int i = 0; i < n; i++)
     {
         y1 = a * y1 + b * u;
-        y2 = a * y2 - b * y2 * y2 + c * u + d * sin(u);
-        cout << "Result of the " << i + 1 << "st step of linear model: " << y1 << ";\n";
-        cout << "Result of the " << i + 1 << "st step of non-linear model: " << y2 << ";\n";
+        y2 = a * y2 - b * y2 * y2 + c * u + d * std::sin(u);
+        std::cout << "Result of the " << i + 1 << "st step of linear model: " << y1 << ";\n";
+        std::cout << "Result of the " << i + 1 << "st step of non-linear model: " << y2 << ";\n";
     }
 
     return 0;
