7cce25f263b06e522331cb6a4c0867616825af62
cyber-baka-cmyk <khalisiakadeny@gmail.com>
2025-10-01 19:31:20 +0300

    fix_1

--- trunk/ii02726/task_01/src/otis_task_01.cpp
+++ trunk/ii02726/task_01/src/otis_task_01.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 
-
 int main() {
-    int max_steps= 100;
-    int steps;
-    double a, b, c, d;
-    long double y[max_steps];
-    double u[max_steps];
+    int max_steps = 100;
 
+    int steps;
     std::cout << "enter steps: ";
     std::cin >> steps;
+
     if (steps > max_steps || steps < 2) {
-        std::cout << "incorreect: \n";
+        std::cout << "incorrect: \n";
         return 1;
     }
 
+    double a;
+    double b;
+    double c;
+    double d;
+
     std::cout << "enter a: \n";
     std::cin >> a;
+
     std::cout << "enter b: \n";
     std::cin >> b;
+
     std::cout << "enter c: \n";
     std::cin >> c;
+
     std::cout << "enter d: \n";
     std::cin >> d;
+
+    std::vector<long double> y(steps + 1);
+    std::vector<double> u(steps);
+
     std::cout << "enter y0: \n";
     std::cin >> y[0];
-    
-    std::cout << "entter y1: \n";
 
+    std::cout << "enter y1: \n";
     std::cin >> y[1];
 
     std::cout << "enter u: \n";
@@ -38,14 +47,14 @@ int main() {
     }
 
     int model;
-    std::cout << "choose model: 1-linear; 2-non-linear";
+    std::cout << "choose model: 1-linear; 2-non-linear\n";
     std::cin >> model;
 
     for (int t = 1; t < steps; t++) {
         if (model == 1) {
             y[t + 1] = a * y[t] + b * u[t];
         } else {
-            y[t + 1] = a * y[t] - b * y[t - 1] * y[t - 1] + c * u[t] + d * sin(u[t - 1]);
+            y[t + 1] = a * y[t] - b * y[t - 1] * y[t - 1] + c * u[t] + d * std::sin(u[t - 1]);
         }
     }
 
