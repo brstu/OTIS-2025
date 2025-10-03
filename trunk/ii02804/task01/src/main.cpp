0ea5610fdc0101035b2578ae6494b649e87e04ad
Timofey <bytimofey@gmail.com>
2025-10-03 19:40:46 +0300

    copilot changes

--- trunk/ii02804/task01/src/main.cpp
+++ trunk/ii02804/task01/src/main.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 void simulateLinearModel(int n, double a, double b, double u0, double y0) {
     double y = y0;
 
@@ -14,17 +13,24 @@ void simulateLinearModel(int n, double a, double b, double u0, double y0) {
     }
 }
 
-
 void simulateNonlinearModel(int n, double a, double b, double c, double d, double u0, double y0) {
     double y = y0;
     double y_prev = y0;
+    double u_prev = u0;  
 
     cout << "\n--- Nonlinear Model ---\n";
     for (int tau = 0; tau < n; ++tau) {
         cout << "Time " << tau + 1 << ": " << y << endl;
-        double y_next = a * y - b * pow(y_prev, 2) + c * u0 + d * sin(u0);
+
+        double u;
+        cout << "Enter heat input at time " << tau + 1 << ": ";
+        cin >> u;
+
+        double y_next = a * y - b * pow(y_prev, 2) + c * u + d * sin(u_prev);
+
         y_prev = y;
         y = y_next;
+        u_prev = u;  
     }
 }
 
@@ -33,23 +39,20 @@ int main() {
     double u0;
     double y0;
 
-    
     cout << "Enter number of time steps n: ";
     cin >> n;
 
-    cout << "Enter input warm u0: ";
+    cout << "Enter initial heat input u0: ";
     cin >> u0;
 
     cout << "Enter initial temperature y0: ";
     cin >> y0;
 
-   
     double a = 0.9;
     double b = 0.1;
     double c = 0.05;
     double d = 0.02;
 
-   
     simulateLinearModel(n, a, b, u0, y0);
     simulateNonlinearModel(n, a, b, c, d, u0, y0);
 
