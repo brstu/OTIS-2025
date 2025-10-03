b1d82ffdf00ed41d03d9d396196d92c6311f12ea
Kostya <konatantinkanevega@gmail.com>
2025-10-03 08:51:58 +0300

    Redaction main.cpp (fot SonarQube)

--- trunk/ii02810/task_01/src/main.cpp
+++ trunk/ii02810/task_01/src/main.cpp
@@ -3,19 +3,30 @@
 
 using namespace std;
 
-void Linemodel(float y, float u, float t, float a, float b, int i) {
-    if (i <= t) {
+struct Params {
+    float a;
+    float b;
+    float c;
+    float d;
+    float u;
+    int t;
+};
+
+void Linemodel(float y, float u, int t, float a, float b, int i = 1) {
+    if (i <= steps) {
         Linemodel(a * y + b * u, u, t, a, b, i + 1);
         cout << "y" << i << " = " << y << endl;
     }
-    else cout << "end_Linemodel\n";
+    else {
+        cout << "end_Linemodel\n";
+    }
 }
 
-void UnLinemodel(float a, float b, float c, float d, float y, float y0, float u, int i, float t) {
-    if (i <= t) {
-        float y_next = a * y - b * y0 * y0 + c * u + d * sin(u);
+void UnLinemodel(const Params& p, float y, float y0, int i = 1) {
+    if (i <= p.t) {
+        float y_next = p.a * y - p.b * y0 * y0 + p.c * p.u + p.d * sin(p.u);
 
-        UnLinemodel(a, b, c, d, y_next, y, u, i + 1, t);
+        UnLinemodel(p, y_next, y, i + 1);
 
         cout << "y" << i << " = " << y << endl;
     }
@@ -25,11 +36,21 @@ void UnLinemodel(float a, float b, float c, float d, float y, float y0, float u,
 }
 
 int main() {
-    float a, b, c, d, y, u, t;
-    int i = 1;
+    float a;
+    float b;
+    float c;
+    float d;
+    float y;
+    float u;
+    int t;
+
     cout << "Linemodel: Enter constants (a, b, y, u, t) separated by spaces:\n";
     cin >> a >> b >> y >> u >> t;
-    Linemodel(y, u, t, a, b, i);
+    Linemodel(y, u, t, a, b);
+
     cout << "UnLinemodel: Enter constants (a, b, c, d, y, u, t) separated by spaces:\n";
     cin >> a >> b >> c >> d >> y >> u >> t;
+
+    Params p{ a, b, c, d, u, t };
+    UnLinemodel(p, y, y);
 }
