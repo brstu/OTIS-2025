aed979535d788f720da3aae1611ba9951989d4fe
lusha <lusha11q11@gmail.com>
2025-10-04 13:30:37 +0300

    feat: add README.md file to doc & update main.cpp.

--- trunk/ii02702/task_01/src/main.cpp
+++ trunk/ii02702/task_01/src/main.cpp
@@ -10,12 +10,14 @@ void simulateLinearModel(double y, double u, int t, double a, double b) {
 }
 
 // Функция симуляции нелинейной модели
-void simulateNonLinearModel(double y, double u, int t, double a, double b, double c, double d, double u_step) {
+void simulateNonLinearModel(double y, double u, int t, double a, double b, double c, double d) {
     // Инициализируем предыдущие выходные и входные значения с помощью смещений.
     // Смещения (yOffset и uOffset) представляют собой разницу между начальным и предыдущим состояниями,
     // позволяя нелинейной модели начать с заданной истории для y и u.
     const double y_offset = 0.1;
     const double u_offset = 0.1;
+
+    const double u_step = 0.5;       // шаг изменения для переменной u в нелинейной модели
 
     double prevY = y - y_offset;
     double prevU = u - u_offset;
@@ -44,14 +46,13 @@ int main() {
     double nonlinear_b = 0.5;  // коэффициент при квадрате предыдущего значения y (нелинейность)
     double nonlinear_c = 0.3;  // коэффициент при входном воздействии u
     double nonlinear_d = 0.5;  // коэффициент при синусоидальном члене
-    double u_step = 0.5;       // шаг изменения для переменной u в нелинейной модели
-
+    
     std::cout << "Linear simulation:" << std::endl;
     simulateLinearModel(y, u, t, linear_a, linear_b);
     std::cout << std::endl;
 
     std::cout << "Nonlinear simulation:" << std::endl;
-    simulateNonLinearModel(y, u, t, nonlinear_a, nonlinear_b, nonlinear_c, nonlinear_d, u_step);
+    simulateNonLinearModel(y, u, t, nonlinear_a, nonlinear_b, nonlinear_c, nonlinear_d);
     std::cout << std::endl;
 
     return 0;
