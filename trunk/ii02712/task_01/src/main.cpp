80a756636f2b11c800b2b115fc1f2ebf68c294f2
Литовкин Никита <tradevip598@gmail.com>
2025-10-06 17:24:09 +0300

    all good

--- trunk/ii02712/task_01/src/main.cpp
+++ trunk/ii02712/task_01/src/main.cpp
@@ -17,31 +17,29 @@ double nonlinearModel(double y, double u, double y_pred, double u_pred) {
   
 int main() {
     double y;
-    std::cout << "Enter the initial temperature : ";
+    std::cout << "Enter the initial temperature: ";
     std::cin >> y;
     double y_next = y;
     double y_pred;
-    double u = 8;
+    const double u = 8 ;
     double u_pred = u;
-    int steps = 10;
+    const int steps = 10;
 
     std::cout << "The Linear model" << std::endl;
     std::cout << "Time:\tTemperature:" << std::endl;
     for (int i = 0; i < steps; i++) {
-        y_pred = y_next;
         y_next = linearModel(y_next, u);
         std::cout << i + 1 << "\t" << y_next << std::endl;
     }
     std::cout << std::endl;
     y_next = y;
-    y_pred = y;
     std::cout << "Nonlinear model:" << std::endl;
     std::cout << "Time:\tTemperature:" << std::endl;
     for (int i = 0; i < steps; i++) {
         y_pred = y_next;
+        u_pred = u ; 
         y_next = nonlinearModel(y_next, u, y_pred, u_pred);
         std::cout << i + 1 << "\t" << y_next << std::endl; 
-        u_pred = u;
     }
     return 0;
 }
