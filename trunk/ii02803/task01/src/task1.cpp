26d47db693ebcd4ec54588583611e296be304037
Ilay <ilay.vabischevich@gmail.com>
2025-10-08 13:40:12 +0300

    7 пар

--- trunk/ii02803/task01/src/task1.cpp
+++ trunk/ii02803/task01/src/task1.cpp
@@ -13,19 +13,39 @@ vector<double> fun1(int n, double a, double b, const vector<double>& u, double y
     return y;
 }
 
-vector<double> fun2(int n, double a, double b, double c, double d, double e, double f, double g,
-                    const vector<double>& u, double y0, double y1) {
+vector<double> fun2(double a, double b, double c, double d, double e, double f, double g) {
+    int n = 5;
+    vector<double> u(n + 1);
+    u[0] = 1.0;
+    u[1] = 1.0;
+    u[2] = 1.0;
+    u[3] = 1.0;
+    u[4] = 1.0;
+    u[5] = 0.0;
+    
+    double y0 = 20.0;
+    double y1 = 20.5;
+    
     vector<double> y(n + 1);
     y[0] = y0;
     y[1] = y1;
 
     for (int t = 1; t < n; t++) {
-        y[t + 1] = a * y[t] + 
-                   b * (y[t - 1] * y[t - 1]) + 
-                   c * u[t] + 
-                   d * sin(u[t - 1]) + 
-                   e * (u[t] * u[t]) + 
-                   f * cos(y[t]) + 
+        double current_y = y[t];
+        double previous_y = y[t - 1];
+        double current_u = u[t];
+        double previous_u = u[t - 1];
+        double squared_previous_y = previous_y * previous_y;
+        double squared_current_u = current_u * current_u;
+        double sin_previous_u = sin(previous_u);
+        double cos_current_y = cos(current_y);
+        
+        y[t + 1] = a * current_y + 
+                   b * squared_previous_y + 
+                   c * current_u + 
+                   d * sin_previous_u + 
+                   e * squared_current_u + 
+                   f * cos_current_y + 
                    g;
     }
     return y;
@@ -73,7 +93,7 @@ int main() {
     cin >> y1;
 
     vector<double> yLinear = fun1(n, a_lin, b_lin, u, y0);
-    vector<double> yNonlinear = fun2(n, a_nlin, b_nlin, c_nlin, d_nlin, e_nlin, f_nlin, g_nlin, u, y0, y1);
+    vector<double> yNonlinear = fun2(a_nlin, b_nlin, c_nlin, d_nlin, e_nlin, f_nlin, g_nlin);
 
     cout << "Time\tLinear Model\tNonlinear Model\n";
     cout << fixed;
