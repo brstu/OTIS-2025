a1ee39f1bebf168ada7bccb470e71a4e029e255e
Ilay <ilay.vabischevich@gmail.com>
2025-10-08 13:32:34 +0300

    Define each identifier in a dedicated statement

--- trunk/ii02803/task01/src/task1.cpp
+++ trunk/ii02803/task01/src/task1.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 vector<double> fun1(int n, double a, double b, const vector<double>& u, double y0) {
     vector<double> y(n + 1);
     y[0] = y0;
@@ -33,18 +32,32 @@ vector<double> fun2(int n, double a, double b, double c, double d, double e, dou
 }
 
 int main() {
-    int n; 
-    double a_lin, b_lin; 
-    double a, b, c, d, e, f, g;
+    int n;
+    double a_lin;
+    double b_lin;
+    double a_nlin;
+    double b_nlin;
+    double c_nlin;
+    double d_nlin;
+    double e_nlin;
+    double f_nlin;
+    double g_nlin;
 
     cout << "Number of steps n: ";
     cin >> n;
 
     cout << "Parameters for linear model (a, b): ";
-    cin >> a_lin >> b_lin;
+    cin >> a_lin;
+    cin >> b_lin;
 
     cout << "Parameters for nonlinear model (a, b, c, d, e, f, g): ";
-    cin >> a >> b >> c >> d >> e >> f >> g;
+    cin >> a_nlin;
+    cin >> b_nlin;
+    cin >> c_nlin;
+    cin >> d_nlin;
+    cin >> e_nlin;
+    cin >> f_nlin;
+    cin >> g_nlin;
 
     vector<double> u(n + 1);
     cout << "Enter " << n << " values of the input signal u (u[0] to u[" << n-1 << "]):\n";
@@ -53,12 +66,14 @@ int main() {
     }
     u[n] = 0.0;
 
-    double y0, y1;
+    double y0;
+    double y1;
     cout << "Initial conditions y0 and y1: ";
-    cin >> y0 >> y1;
+    cin >> y0;
+    cin >> y1;
 
-    auto yLinear = fun1(n, a_lin, b_lin, u, y0);
-    auto yNonlinear = fun2(n, a, b, c, d, e, f, g, u, y0, y1);
+    vector<double> yLinear = fun1(n, a_lin, b_lin, u, y0);
+    vector<double> yNonlinear = fun2(n, a_nlin, b_nlin, c_nlin, d_nlin, e_nlin, f_nlin, g_nlin, u, y0, y1);
 
     cout << "Time\tLinear Model\tNonlinear Model\n";
     cout << fixed;
