23ad9e414a6dcad2f22f16ce073b8bcc2826f5c9
Boss <artemmasuk732@gmail.com>
2025-10-24 22:20:04 +0300

    Some changes at test.cpp

--- trunk/ii02714/task_02/test/test.cpp
+++ trunk/ii02714/task_02/test/test.cpp
@@ -1,8 +1,40 @@
-﻿#include "C:\Users\Boss\source\repos\otis2\otis2\Solution.h"
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+#include "C:/Users/Boss/source/repos/otis2/otis2/Solution.h"
+#include "pch.h"
 
-int main()
-{
-    
+TEST(TestClass, linearfun1) {
+	Obj o;
+	o.y.at(0) = 1.0;
+	o.u.at(0) = 2.5;
+	o.linear(0);
+	double res = o.y.at(1); 
+    EXPECT_EQ(res,3.5);
 }
-
+TEST(TestClass, linearfun2) {
+	Obj o;
+	o.y.at(0) = -2.0;
+	o.u.at(0) = 3.5;
+	o.linear(0);
+	double res = o.y.at(1);
+	EXPECT_EQ(res, 1.5);
+}
+TEST(Test2Class, Nonlinearfun1) {
+	Obj o;
+	o.y.at(0) = 1.0;
+	o.y.at(1) = 3.0;
+	o.u.at(0) = 2.5;
+	o.u.at(1) = 0.5;
+	o.nonlinear(1);
+	double res = o.y.at(2);
+	EXPECT_NEAR(res, 3,0.1);
+}
+TEST(Test2Class, Nonlinearfun2) {
+	Obj o;
+	o.y.at(0) = 0.0;
+	o.y.at(1) = 7.0;
+	o.u.at(0) = -4.5;
+	o.u.at(1) = -0.5;
+	o.nonlinear(1);
+	double res = o.y.at(2);
+	EXPECT_NEAR(res, 7,0.5);
+}
