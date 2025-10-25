49879895fcc3984e5c9d8fd628e816810f632729
Aleksey <alexei.tsitunin@gmail.com>
2025-10-25 15:05:46 +0300

    Init task_02

---
+++ trunk/ii02719/task_02/src/functions.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void linear(double a, double b, double* y, double* u, const int steps);
+void nonLinear(double a, double b, double c, double d, double* y, double* u, const int steps);
---
+++ trunk/ii02719/task_02/src/main.cpp
@@ -0,0 +1,47 @@
+﻿#include "functions.h"
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	const int steps = 10;
+	double y[steps];
+	double u[steps];
+	double a;
+	double b;
+	double c;
+	double d;
+
+	std::cout << "Рассчет по линейной модели\n";
+	std::cout << "Введите коэффициент a: ";
+	std::cin >> a;
+	std::cout << "Введите коэффициент b: ";
+	std::cin >> b;
+	std::cout << "Введите начальную температуру объекта y(0): ";
+	std::cin >> y[0];
+	std::cout << "Введите начальную температуру объекта u(0): ";
+	std::cin >> u[0];
+
+    linear(a, b, y, u, steps);
+
+	std::cout << "Рассчет по нелинейной модели\n";
+	std::cout << "Введите коэффициент a: ";
+	std::cin >> a;
+	std::cout << "Введите коэффициент b: ";
+	std::cin >> b;
+	std::cout << "Введите коэффициент c: ";
+	std::cin >> c;
+	std::cout << "Введите коэффициент d: ";
+	std::cin >> d;
+	std::cout << "Введите начальную температуру объекта y(0): ";
+	std::cin >> y[0];
+	std::cout << "Введите следующую температуру объекта y(1): ";
+	std::cin >> y[1];
+	std::cout << "Введите начальную температуру объекта u: ";
+	std::cin >> u[0];
+	u[1] = u[0] * 0.8; //Коэффициент понижения температуры объекта
+
+	nonLinear(a, b, c, d, y, u, steps);
+}
