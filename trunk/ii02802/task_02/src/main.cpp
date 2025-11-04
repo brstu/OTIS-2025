38c81df54715dc626adf1f19a73aa27cd4b6ac45
Daniil <microlasel@gmail.com>
2025-11-04 21:18:32 +0300

    Выполение второй лаборатоной работы

--- trunk/ii02802/task_02/src/main.cpp
+++ trunk/ii02802/task_02/src/main.cpp
@@ -1,17 +1,8 @@
 #include  <iostream>
 #include  <cmath>
+#include  "functions.h"
 
 using  namespace  std;
-
-double linear(double  y, double  u, double  a, double  b)
-{
-    return  a  *  y  +  b  *  u;
-}
-
-double nonlinear(double  y, double  u, double  a, double  b, double  c, double  d)
-{
-    return  a  *  y  -  b  *  y  *  y  +  c  *  u  +  d  *  sin(u);
-}
 
 int  main()
 {
