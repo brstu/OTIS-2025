38ab2d9b8bfa9d1b8f80bb704a5213f2d111a351
Timofey <bytimofey@gmail.com>
2025-11-07 11:15:18 +0300

    changes

--- trunk/ii02804/task_02/src/task_01.cpp
+++ trunk/ii02804/task_02/src/task_01.cpp
@@ -1,16 +1,16 @@
 #include "task_01.h"
 #include <cmath>
 
-double linear_model_ii02804(double y, double u, double a, double b) {
-    double result = a * y + b * u;
-    return result;
+// Линейная модель
+double linear_model(double y, double u, double a, double b) {
+    return a * y + b * u;
 }
 
-double non_linear_model_ii02804(double y, double& y_previous, double u, double& u_previous, double a, double b, double c, double d) {
-    y_previous = y;
+// Нелинейная модель 
+double non_linear_model(double y, double& y_prev, double u, double& u_prev, double a, double b, double c, double d) {
+    y_prev = y;  
     double linear_part = a * y + c * u;
-    double nonlinear_part = -b * y * y + d * std::sin(u_previous);
-    u_previous = u;
-    double final_result = linear_part + nonlinear_part;
-    return final_result;
+    double nonlinear_part = -b * y * y + d * std::sin(u); 
+    u_prev = u;
+    return linear_part + nonlinear_part;
 }
--- trunk/ii02804/task_02/src/task_01.h
+++ trunk/ii02804/task_02/src/task_01.h
@@ -1,4 +1,4 @@
 #pragma once
 
-double linear_model_ii02804(double y, double u, double a, double b);
-double non_linear_model_ii02804(double y, double& y_previous, double u, double& u_previous, double a, double b, double c, double d);
+double linear_model(double y, double u, double a, double b);
+double non_linear_model(double y, double& y_prev, double u, double& u_prev, double a, double b, double c, double d);
