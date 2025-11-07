81e45f6f3a1e5f63cd25940765b479b1909263ed
Timofey <bytimofey@gmail.com>
2025-11-07 10:27:00 +0300

    changes

--- trunk/ii02804/task_02/src/task_01.h
+++ trunk/ii02804/task_02/src/task_01.h
@@ -2,4 +2,4 @@
 
 
 double linear_model(double y, double u, double a, double b);
-double non_linear_model(double y, double& y_prev, double u, double a, double b, double c, double d);
+double non_linear_model(double y, double& y_prev, double u, double& u_prev, double a, double b, double c, double d);
