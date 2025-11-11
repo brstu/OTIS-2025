e5399e90527355b592d4d6a2c3155cab83b2f390
Kostya <konatantinkanevega@gmail.com>
2025-11-11 15:51:07 +0300

    redaction all needed files

--- trunk/ii02810/task_02/src/main.cpp
+++ trunk/ii02810/task_02/src/main.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 
 int main() {
-    float a, b, c, d, y, u;
-    int steps;
+    float a, b, c, d, y, y0, u;
+    int steps, linemodel_result, result;
 
     cout << "Linemodel: Enter constants (a, b, y, u, steps): ";
     cin >> a >> b >> y >> u >> steps;
-    Linemodel(y, u, steps, a, b);
+    auto linemodel_result = Linemodel(y, u, steps, a, b);
+    cout << "Linemodel result: "<< linemodel_result << endl;
 
-    cout << "UnLinemodel: Enter constants (a, b, c, d, y, u, steps): ";
-    cin >> a >> b >> c >> d >> y >> u >> steps;
+    cout << "UnLinemodel: Enter constants (a, b, c, d, y, y0, u, steps): ";
+    cin >> a >> b >> c >> d >> y >> y0 >> u >> steps;
 
     Params p{ a, b, c, d, u, steps };
-    UnLinemodel(p, y, y);
-    return 0;
+    auto result = UnLinemodel(p, y, y0);
+    cout << "UnLinemodel result: " << result << endl;
 }
