0d4cc88598f53dd2aa07c7c2b65d27890cb6732b
JohnyDepp228 <artemmasuk732@gmail.com>
2025-11-11 11:55:48 +0300

    CmakeLists required

--- trunk/ii02714/task_03/src/PID.h
+++ trunk/ii02714/task_03/src/PID.h
@@ -6,84 +6,120 @@
 
 class PID : public Obj {
 private:
-    std::vector<double> e;
-    std::vector<double> yP;
-    std::vector<double> w;//целевое значение
-    std::vector<double> u;
-    std::vector<double> deltU;
-    double Td;//постоянная дефф
-    double To;//шаг по кванту
-    double T;//шаг
-    double q0;
-    double q1;
-    double q2;
-    double k;//кэф передачи
-    int iter;
+	std::vector<double> e;
+	std::vector<double> yP;
+	std::vector<double> w;//целевое значение
+	std::vector<double> u;
+	std::vector<double> deltU;
+	double Td;//постоянная дефф
+	double To;//шаг по кванту
+	double T;//шаг
+	double q0;
+	double q1;
+	double q2;
+	double k;//кэф передачи
+	int iter;
 public:
-    PID() {
-        this->iter = 10;
-        this->T = 0.01;
-        this->To = 0.01;
-        this->k = 0.1;
-        this->Td = 1;
-        this->u[0] = 0;
-        ++iter;
-        e.resize(iter);
-        yP.resize(iter);
-        w.resize(iter);
-        u.resize(iter);
-        deltU.resize(iter);
-        setE(0);
-        setE(1);
-        setE(2);
-        Obj::input();
-        SetQs();
-        for (auto meanW : w) {
-            std::cout << "Enter w:" << std::endl;
-            std::cin >> meanW;
-        }
-    }
-    void SetY(int place) {
-        Obj::linear(place);
-        this->yP[place] = getY(place);
-    }
-    void SetY(int place,double data) {
-        this->yP[place] = data;
-    }
-    void setE(int place) {
-        e[place] = w[place] - yP[place];
-    }
-    void SetQs() {
-        this->q0 = k * (1 + Td / To);
-        this->q1 = -k * (1 + 2 * (Td / To) - To / T);
-        this->q2 = k * (Td / To);
-    }
-    void SetU(int place) {
-        int placeE = 0;
-        if (place == 0 || place == 1) {
-            placeE = 2;
-        }
-        if (place > 2) {
-            setE(place);
-        }
-        deltU[place] = q0 * e[placeE] + q1 * e[placeE - 1] + q2 * e[placeE - 2];
-        u[place] = u[place - 1] + deltU[place];
-    }
-    int iterGet() {
-        --iter;
-        return iter;
-    }
-    double GetE(int place) {
-        return this->e[place];
-    }
-    double GetYp(int place) {
-        return this->yP[place];
-    }
-    double GetUP(int place) {
-        return this->u[place];
-    }
-    void SetW(int place,double data) {
-        this->w[place] = data;
-    }
+	PID(double data,double data2) {
+		this->iter = 10;
+		e.resize(iter);
+		yP.resize(iter);
+		w.resize(iter);
+		u.resize(iter);
+		deltU.resize(iter);
+	    SetW(3, data);
+        SetY(3, data2);
+		this->T = 0.01;
+		this->To = 0.01;
+		this->k = 0.1;
+		this->Td = 1;
+		this->u[0] = 1;
+		this->e[0] = 0;
+		this->e[1] = 0;
+		this->e[2] = 0;
+	}
+	PID() {
+		this->iter = 10;
+		e.resize(iter);
+		yP.resize(iter);
+		w.resize(iter);
+		u.resize(iter);
+		deltU.resize(iter);
+		this->T = 0.01;
+		this->To = 0.01;
+		this->k = 0.1;
+		this->Td = 1;
+		this->u[0] = 0;
+		this->e[0] = 0;
+		this->e[1] = 0;
+		this->e[2] = 0;
+		Obj::input();
+		SetQs();
+	}
+	void SetW() {
+		for (int i = 0; i < 10; i++) {
+			std::cout << "Enter w:" << std::endl;
+			std::cin >> w[i];
+		}
+	}
+	void SetY(int place) {
+		Obj::linear(place);
+		this->yP[place] = getY(place);
+	}
+	void SetY(int place, double data) {
+		this->yP[place] = data;
+	}
+	void setE(int place) {
+		e[place] = w[place] - yP[place];
+	}
+	void SetQs() {
+		this->q0 = k * (1 + Td / To);
+		this->q1 = -k * (1 + 2 * (Td / To) - To / T);
+		this->q2 = k * (Td / To);
+	}
+	void SetU(int place) {
+		int placeE = 0;
+		int placeQ = 0;
+		if (place == 0) {
+			placeQ = 1;
+		}
+		else {
+			placeQ = place;
+		}
+
+		if (place == 0 || place == 1) {
+			placeE = 2;
+		}
+		else if (place > 2) {
+			setE(place);
+		}
+		deltU[place] = q0 * e[placeE] + q1 * e[placeE - 1] + q2 * e[placeE - 2];
+		u[placeQ] = u[placeQ - 1] + deltU[place];
+	}
+	int iterGet() {
+		--iter;
+		return iter;
+	}
+	double GetE(int place) {
+		return this->e[place];
+	}
+	double GetYp(int place) {
+		return this->yP[place];
+	}
+	double GetUP(int place) {
+		return this->u[place];
+	}
+	void SetW(int place, double data) {
+		this->w[place] = data;
+	}
+	double GetQs0() {
+		return this->q0;
+	}
+	double GetQs1() {
+		return this->q1;
+	}
+	double GetQs2() {
+		return this->q2;
+	}
 
 };
--- trunk/ii02714/task_03/test/test.cpp
+++ trunk/ii02714/task_03/test/test.cpp
@@ -17,12 +17,42 @@ TEST(TestClass, linearfun2) {
 	o.SetU(0, 3.5);
 	o.linear(0);
 	double res = o.getY(1);
-	EXPECT_NEAR(res, 1.5, 0.1);
+	EXPECT_NEAR(res, 1.5, 0.01);
 }
-TEST(TESTClass, funE) {
-	PID p;
-	p.SetW(3, 13);
-	p.SetY(3,12.5);
+TEST(TESTClass, funE1) {
+	PID p(13,12.5);
+	p.setE(3);
 	double res = p.GetE(3);
-	EXPECT_NEAR(res, 0.5, 0.1);
+	EXPECT_NEAR(res, 0.5, 0.01);
+}
+TEST(TESTClass, funE2) {
+	PID p(2.5, 1.5);
+	p.setE(3);
+	double res = p.GetE(3);
+	EXPECT_NEAR(res,1, 0.01);
+}
+TEST(TESTClass, funQ1) {
+	PID p(2.5, 1.5);
+	p.SetQs();
+	double res = p.GetQs0();
+	EXPECT_NEAR(res, 10.1,0.01);
+}
+TEST(TESTClass, funQ2) {
+	PID p(2.5, 1.5);
+	p.SetQs();
+	double res = p.GetQs1();
+	EXPECT_NEAR(res, -20, 0.01);
+}
+TEST(TESTClass, funQ3) {
+	PID p(2.5, 1.5);
+	p.SetQs();
+	double res = p.GetQs2();
+	EXPECT_NEAR(res, 10, 0.01);
+}
+TEST(TESTClass, funU1) {
+	PID p(2.5, 1.5);
+	p.SetQs();
+	p.SetU(0);
+	double res = p.GetUP(1);
+	EXPECT_NEAR(res, 1, 0.01);
 }
