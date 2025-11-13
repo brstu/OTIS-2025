7ff7c7543bf27fe3dc530fed24b0ec7eee37ad9e
Aleksey <alexei.tsitunin@gmail.com>
2025-11-13 19:15:19 +0300

    Working with lab3

---
+++ trunk/ii02719/task_03/src/functions.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <vector>
+
+void linear(double a, double b, std::vector<double>& y, const int steps);
---
+++ trunk/ii02719/task_03/src/main.cpp
@@ -0,0 +1,25 @@
+﻿#include "functions.h"
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	const int steps = 30;
+	std::vector<double> y;
+	y.resize(steps);
+	double a = 0.8;
+	double b = 0.5;
+
+	std::cout << "Рассчет по линейной модели\n";
+	std::cout << "Введите начальную температуру объекта y(0): ";
+	std::cin >> y[0];
+
+    linear(a, b, y, steps);
+
+	std::cout << "Нажмите Enter для выхода...";
+	std::cin.ignore();
+	std::cin.get();
+}
