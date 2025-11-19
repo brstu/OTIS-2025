a375e5e0b650c36c4e3ef6959c1613ed1215d1d2
ttevvss <derkach.20@bk.ru>
2025-11-19 11:25:29 +0300

    устранила некоторые ошибки

--- build_test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/MSI/Documents/OTIS-2025/trunk/ii02807/task_03/test
-# Build directory: C:/Users/MSI/Documents/OTIS-2025/build_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/MSI/Documents/OTIS-2025/build_test/task_03_ii02807_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- build_test/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/MSI/Documents/OTIS-2025/build_test/_deps/googletest-src/googletest
-# Build directory: C:/Users/MSI/Documents/OTIS-2025/build_test/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
