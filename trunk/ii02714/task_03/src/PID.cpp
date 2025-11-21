06a92615723673b075fb195e59d96e729d69ec99
Масюк Артем Денисович <artemmasuk732@gmail.com>
2025-11-21 18:59:22 -0100

    deleted private fields

--- trunk/ii02714/task_03/src/PID.cpp
+++ trunk/ii02714/task_03/src/PID.cpp
@@ -6,20 +6,6 @@
 
 
 class PID : public Obj {
-private:
-	std::vector<double> e;
-	std::vector<double> yP;
-	std::vector<double> w;//целевое значение
-	std::vector<double> u;
-	std::vector<double> deltU;
-	double Td;//постоянная дефф
-	double To;//шаг по кванту
-	double T;//шаг
-	double q0;
-	double q1;
-	double q2;
-	double k;//кэф передачи
-	int iter;
 public:
 	PID(double data, double data2) {
 		this->iter = 10;
