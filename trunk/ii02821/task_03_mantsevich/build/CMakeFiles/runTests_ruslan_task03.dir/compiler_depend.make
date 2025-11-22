9592bcbffe1d06b594e4d8798c815daafadb0755
humoristoff <bmw.dimon1994@gmail.com>
2025-11-23 01:27:56 +0300

    Changed files

--- trunk/ii02821/task_03_mantsevich/build/CMakeFiles/runTests_ruslan_task03.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for runTests_ruslan_task03.
-# This may be replaced when dependencies are built.
--- trunk/ii02821/task_03_mantsevich/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/study/otis/OTIS-2025/trunk/ii02821/task_03_mantsevich/build/_deps/googletest-src/googletest
-# Build directory: D:/study/otis/OTIS-2025/trunk/ii02821/task_03_mantsevich/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
