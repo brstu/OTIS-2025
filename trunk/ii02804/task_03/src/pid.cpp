ace8bfc385d04007fc779dc4fdd370d4a58720fd
Timofey <bytimofey@gmail.com>
2025-11-25 19:26:28 +0300

    add change

--- trunk/ii02804/task_03/src/pid.cpp
+++ trunk/ii02804/task_03/src/pid.cpp
@@ -2,15 +2,12 @@
 #include <iostream>
 
 PID::PID(double in_K, double in_T, double in_Td, double in_T0)
-    : K(in_K), T(in_T), Td(in_Td), T0(in_T0)
+    : K(in_K), T(in_T), Td(in_Td), T0(in_T0), valid(true)  
 {
-    // Вычисляем q0, q1, q2
-    
+    // Вычисляем дискретные коэффициенты
     q0 = K * (1.0 + Td / T0);
     q1 = -K * (1.0 + 2.0 * Td / T0 - T0 / T);
     q2 = K * Td / T0;
-
-    valid = true;
 }
 
 double PID::u_calc(double e) {
@@ -18,14 +15,11 @@ double PID::u_calc(double e) {
         double delta_u = q0 * e + q1 * e_prev_1 + q2 * e_prev_2;
         double u = u_prev + delta_u;
 
-        
         u_prev = u;
         e_prev_2 = e_prev_1;
         e_prev_1 = e;
-
         return u;
     }
-
     std::cout << "Not valid value of 'q'!\n";
     return 0.0;
 }
