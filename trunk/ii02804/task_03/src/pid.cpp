687b681833865cf8a88e412a134ad90a77db4d65
Timofey <bytimofey@gmail.com>
2025-11-25 19:10:56 +0300

    add change

--- trunk/ii02804/task_03/src/pid.cpp
+++ trunk/ii02804/task_03/src/pid.cpp
@@ -2,19 +2,15 @@
 #include <iostream>
 
 PID::PID(double in_K, double in_T, double in_Td, double in_T0)
-    : K(in_K),
-      T(in_T),
-      Td(in_Td),
-      T0(in_T0),
-      q0(K * (1.0 + Td / T0)),
-      q1(-K * (1.0 + 2.0 * Td / T0 - T0 / T)),
-      q2(K * Td / T0),
-      u_prev(0.0),
-      e_prev_1(0.0),
-      e_prev_2(0.0),
-      valid(true)
+    : K(in_K), T(in_T), Td(in_Td), T0(in_T0)
 {
+    // Вычисляем q0, q1, q2
     
+    q0 = K * (1.0 + Td / T0);
+    q1 = -K * (1.0 + 2.0 * Td / T0 - T0 / T);
+    q2 = K * Td / T0;
+
+    valid = true;
 }
 
 double PID::u_calc(double e) {
@@ -22,6 +18,7 @@ double PID::u_calc(double e) {
         double delta_u = q0 * e + q1 * e_prev_1 + q2 * e_prev_2;
         double u = u_prev + delta_u;
 
+        
         u_prev = u;
         e_prev_2 = e_prev_1;
         e_prev_1 = e;
