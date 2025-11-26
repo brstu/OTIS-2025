0865c170cfc1f0fa0896ecf5885c8bd75e260f78
Vladimir <ii002817@g.bstu.by>
2025-11-26 13:39:00 +0300

    fix code1

--- trunk/ii02818/task_01/src/lab1OTIS.cpp
+++ trunk/ii02818/task_01/src/lab1OTIS.cpp
@@ -8,15 +8,17 @@ private:
     double b;
     double c;
     double d;
-    double y_prev;
+    double y_prev;  
     double y_curr;
     double u_prev;
     double u_curr;
 
 public:
     TemperatureModel(double a, double b, double c, double d)
-        : a(a), b(b), c(c), d(d) {
-    }
+        : a(a), b(b), c(c), d(d),  
+        y_prev(0), y_curr(0),   
+        u_prev(0), u_curr(0) {
+    }  
 
     void setInitialState(double y0, double y1, double u0, double u1) {
         y_prev = y0;
