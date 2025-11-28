129cad0ab7acce5b20550180b0f0ff8c591d4258
matuy-g <gurin.matevj@outlook.com>
2025-11-28 08:51:24 +0300

    Initial commit for task_02.

---
+++ trunk/ii02706/task_02/src/functions.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double first_funct_const = 0.1;
+const double second_funct_const = 0.2;
+const double third_funct_const = 0.3;
+
+void liner_funcnt(double& y1);
+void nonliner_funct(double& y2, double& y_previous, bool& second_iteration);
