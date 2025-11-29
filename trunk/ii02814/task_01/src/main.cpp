dbb24d3678178c192c6085920649abc72b3d8f2b
Tlbery <ii002826@g.bstu.by>
2025-11-29 14:13:37 +0300

    Upppp

--- trunk/ii02814/task_01/src/main.cpp
+++ trunk/ii02814/task_01/src/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <cmath>
 
-enum ModelType {
-    Collapse annotationCheck warning on line R4Replace this "enum" with "enum class".Check warning : Replace this "enum" with "enum class".
-    See more on https ://sonarcloud.io/project/issues?id=brstu_OTIS-2025&issues=AZrPQfLT7IxbKjIkqS5I&open=AZrPQfLT7IxbKjIkqS5I&pullRequest=211 / SonarCloud Code AnalysisView detailsCode has alerts. Press enter to view.
-LINEAR,
-NONLINEAR
+enum class ModelType {
+    LINEAR,
+    NONLINEAR
 };
 
 struct LinearModelParams {
@@ -44,11 +42,11 @@ int main()
     SimulationParams simParams = getDefaultSimulationParams();
 
     std::cout << "Linear simulation:" << std::endl;
-    simulateModel(LINEAR, simParams);
+    simulateModel(ModelType::LINEAR, simParams);
     std::cout << std::endl;
 
     std::cout << "Nonlinear simulation:" << std::endl;
-    simulateModel(NONLINEAR, simParams);
+    simulateModel(ModelType::NONLINEAR, simParams);
     std::cout << std::endl;
 
     return 0;
@@ -90,22 +88,21 @@ void simulateNonLinearModel(const SimulationParams& simParams)
     double u = simParams.u;
     int t = simParams.t;
 
-    // Initialize previous output and input values using offsets.
-    // The offsets (yOffset and uOffset) represent the difference between the initial state and the previous state,
-    // allowing the nonlinear model to start with a defined history for y and u.
-    double prevY = y - params.yOffset; Collapse annotationCheck warning on line R94Value stored to 'prevY' during its initialization is never readCheck warning : Value stored to 'prevY' during its initialization is never read
-        See more on https ://sonarcloud.io/project/issues?id=brstu_OTIS-2025&issues=AZrPQfLT7IxbKjIkqS5J&open=AZrPQfLT7IxbKjIkqS5J&pullRequest=211 / SonarCloud Code AnalysisView detailsCode has alerts. Press enter to view.
-    double prevU = u - params.uOffset; Collapse annotationCheck warning on line R95Value stored to 'prevU' during its initialization is never readCheck warning : Value stored to 'prevU' during its initialization is never read
-        See more on https ://sonarcloud.io/project/issues?id=brstu_OTIS-2025&issues=AZrPQfLT7IxbKjIkqS5K&open=AZrPQfLT7IxbKjIkqS5K&pullRequest=211 / SonarCloud Code AnalysisView detailsCode has alerts. Press enter to view.
+    // Initialize previous output and input values
+    double prevY = y;
+    double prevU = u;
 
     for (int i = 0; i <= t; i++)
     {
         std::cout << i << ' ' << y << std::endl;
-        // Update all 'prev' variables first
-        prevY = y;
-        prevU = u;
+
         // Compute nextY using previous values
         double nextY = params.a * y - params.b * prevY * prevY + params.c * u + params.d * sin(prevU);
+
+        // Update previous values for next iteration
+        prevY = y;
+        prevU = u;
+
         // Update current variables
         y = nextY;
         u += params.u_step;
@@ -115,8 +112,8 @@ void simulateNonLinearModel(const SimulationParams& simParams)
 NonLinearModelParams getDefaultNonLinearParams()
 {
     NonLinearModelParams params;
-    params.yOffset = 0.001;  // Offset subtracted from current output to compute previous output (prevY = y - yOffset)
-    params.uOffset = 1;      // Offset subtracted from current input to compute previous input (prevU = u - uOffset)
+    params.yOffset = 0.001;  // Offset for previous output computation
+    params.uOffset = 1;      // Offset for previous input computation
     params.a = 1;            // Linear coefficient for current output (y)
     params.b = 0.5;          // Nonlinear coefficient for squared previous output (prevY²)
     params.c = 0.9;          // Linear coefficient for input (u)
@@ -129,151 +126,10 @@ void simulateModel(ModelType type, const SimulationParams& simParams)
 {
     switch (type)
     {
-    case LINEAR:
+    case ModelType::LINEAR:
         simulateLinearModel(simParams);
         break;
-    case NONLINEAR:
-        simulateNonLinearModel(simParams);
-        break;
-    default:
-        std::cerr << "Error: Invalid ModelType value passed to simulateModel." << std::endl;
-        break;
-    }
-}#include <iostream>
-#include <cmath>
-
-enum ModelType {
-    Collapse annotationCheck warning on line R4Replace this "enum" with "enum class".Check warning : Replace this "enum" with "enum class".
-    See more on https ://sonarcloud.io/project/issues?id=brstu_OTIS-2025&issues=AZrPQfLT7IxbKjIkqS5I&open=AZrPQfLT7IxbKjIkqS5I&pullRequest=211 / SonarCloud Code AnalysisView detailsCode has alerts. Press enter to view.
-LINEAR,
-NONLINEAR
-};
-
-struct LinearModelParams {
-    double a;
-    double b;
-};
-
-struct NonLinearModelParams {
-    double yOffset;
-    double uOffset;
-    double a;
-    double b;
-    double c;
-    double d;
-    double u_step;
-};
-
-struct SimulationParams {
-    double y;
-    double u;
-    int t;
-};
-
-SimulationParams getDefaultSimulationParams();
-
-void simulateLinearModel(const SimulationParams& simParams);
-LinearModelParams getDefaultLinearParams();
-
-void simulateNonLinearModel(const SimulationParams& simParams);
-NonLinearModelParams getDefaultNonLinearParams();
-
-void simulateModel(ModelType type, const SimulationParams& simParams);
-
-int main()
-{
-    SimulationParams simParams = getDefaultSimulationParams();
-
-    std::cout << "Linear simulation:" << std::endl;
-    simulateModel(LINEAR, simParams);
-    std::cout << std::endl;
-
-    std::cout << "Nonlinear simulation:" << std::endl;
-    simulateModel(NONLINEAR, simParams);
-    std::cout << std::endl;
-
-    return 0;
-}
-
-SimulationParams getDefaultSimulationParams()
-{
-    SimulationParams params;
-    params.y = 0.6; // Initial output value
-    params.u = 60;  // Input signal value
-    params.t = 10;  // Simulation time steps
-    return params;
-}
-
-void simulateLinearModel(const SimulationParams& simParams)
-{
-    LinearModelParams params = getDefaultLinearParams();
-    double y = simParams.y;
-
-    for (int i = 0; i <= simParams.t; i++)
-    {
-        std::cout << i << ' ' << y << std::endl;
-        y = params.a * y + params.b * simParams.u;
-    }
-}
-
-LinearModelParams getDefaultLinearParams()
-{
-    LinearModelParams params;
-    params.a = 0.1; // Coefficient for previous output (y)
-    params.b = 0.8; // Coefficient for input (u)
-    return params;
-}
-
-void simulateNonLinearModel(const SimulationParams& simParams)
-{
-    NonLinearModelParams params = getDefaultNonLinearParams();
-    double y = simParams.y;
-    double u = simParams.u;
-    int t = simParams.t;
-
-    // Initialize previous output and input values using offsets.
-    // The offsets (yOffset and uOffset) represent the difference between the initial state and the previous state,
-    // allowing the nonlinear model to start with a defined history for y and u.
-    double prevY = y - params.yOffset; Collapse annotationCheck warning on line R94Value stored to 'prevY' during its initialization is never readCheck warning : Value stored to 'prevY' during its initialization is never read
-        See more on https ://sonarcloud.io/project/issues?id=brstu_OTIS-2025&issues=AZrPQfLT7IxbKjIkqS5J&open=AZrPQfLT7IxbKjIkqS5J&pullRequest=211 / SonarCloud Code AnalysisView detailsCode has alerts. Press enter to view.
-    double prevU = u - params.uOffset; Collapse annotationCheck warning on line R95Value stored to 'prevU' during its initialization is never readCheck warning : Value stored to 'prevU' during its initialization is never read
-        See more on https ://sonarcloud.io/project/issues?id=brstu_OTIS-2025&issues=AZrPQfLT7IxbKjIkqS5K&open=AZrPQfLT7IxbKjIkqS5K&pullRequest=211 / SonarCloud Code AnalysisView detailsCode has alerts. Press enter to view.
-
-    for (int i = 0; i <= t; i++)
-    {
-        std::cout << i << ' ' << y << std::endl;
-        // Update all 'prev' variables first
-        prevY = y;
-        prevU = u;
-        // Compute nextY using previous values
-        double nextY = params.a * y - params.b * prevY * prevY + params.c * u + params.d * sin(prevU);
-        // Update current variables
-        y = nextY;
-        u += params.u_step;
-    }
-}
-
-NonLinearModelParams getDefaultNonLinearParams()
-{
-    NonLinearModelParams params;
-    params.yOffset = 0.001;  // Offset subtracted from current output to compute previous output (prevY = y - yOffset)
-    params.uOffset = 1;      // Offset subtracted from current input to compute previous input (prevU = u - uOffset)
-    params.a = 1;            // Linear coefficient for current output (y)
-    params.b = 0.5;          // Nonlinear coefficient for squared previous output (prevY²)
-    params.c = 0.9;          // Linear coefficient for input (u)
-    params.d = 2.5;          // Nonlinear coefficient for sinusoidal input term
-    params.u_step = 0.01;    // Step size for input signal increment
-    return params;
-}
-
-void simulateModel(ModelType type, const SimulationParams& simParams)
-{
-    switch (type)
-    {
-    case LINEAR:
-        simulateLinearModel(simParams);
-        break;
-    case NONLINEAR:
+    case ModelType::NONLINEAR:
         simulateNonLinearModel(simParams);
         break;
     default:
