6b13c1eb35987c14c8d472b4c98baa55c54892df
Timofey <bytimofey@gmail.com>
2025-12-01 21:57:15 +0300

    add change

--- trunk/ii02712/task_02/src/functions.cpp
+++ trunk/ii02712/task_02/src/functions.cpp
@@ -1,10 +1,16 @@
 #include "functions.h"
 #include <cmath>
 
-double linearModel(double y, double u) {
-    return a * y + b * u;
+/**
+ * @brief Линейная модель с передачей коэффициентов через структуру.
+ */
+double linearModel(double y, double u, const LinearParams& p) {
+    return p.a * y + p.b * u;
 }
 
-double nonlinearModel(double y, double u, double y_pred, double u_pred) {
-    return a * y - b * y * y + c * u + d * std::sin(u);
+/**
+ * @brief Нелинейная модель с передачей коэффициентов через структуру.
+ */
+double nonlinearModel(double y, double y_prev, double u, double u_prev, const NonlinearParams& p) {
+    return p.a * y - p.b * std::pow(y_prev, 2.0) + p.c * u + p.d * std::sin(u_prev);
 }
