1129a2aa43c1d250666d1e1472cbcd337a7da6a6
Гридчин Александр <gridchin228@gmail.com>
2025-12-02 13:02:21 +0200

    removed using namespace

--- trunk/ii02704/task_02/src/linear.h
+++ trunk/ii02704/task_02/src/linear.h
@@ -1,11 +1,10 @@
 #ifndef LIN_H
 #define LIN_H
+
 #include <iostream>
 #include <vector>
 
-using namespace std;
+void input(int& k, std::vector<double>& u, std::vector<double>& y, double& a, double& b);
+void linear(const int& place, const int& k, std::vector<double>& y, const std::vector<double>& u, const double& b, const double& a);
 
-void input(int& k, vector<double> &u, vector<double> &y, double& a, double& b);
-void linear(const int& place, const int& k, vector<double>& y, const vector<double>& u, const double& b, const double& a);
-
-#endif 
+#endif
