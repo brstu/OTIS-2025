8b0f515c971a369dddad58fc1cf83bfdfa08e2a6
JohnyDepp228 <artemmasuk732@gmail.com>
2025-12-02 10:41:30 +0200

    task2 done

---
+++ trunk/ii02704/task_02/src/linear.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef LIN_H
+#define LIN_H
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void input(int& k, vector<double> &u, vector<double> &y, double& a, double& b);
+void linear(const int& place, const int& k, vector<double>& y, const vector<double>& u, const double& b, const double& a);
+
+#endif 
