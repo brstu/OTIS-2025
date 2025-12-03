88e2d1899ce32dfb973b102bf739382cc661a5b3
Timofey <bytimofey@gmail.com>
2025-12-03 14:33:19 +0300

    add change

--- trunk/ii02804/task_03/src/pid.cpp
+++ trunk/ii02804/task_03/src/pid.cpp
@@ -1,25 +1,62 @@
 #include "pid.h"
 #include <iostream>
 
-PID::PID(double in_K, double in_T, double in_Td, double in_T0)
-    : K(in_K), T(in_T), Td(in_Td), T0(in_T0), valid(true)  
+namespace
 {
-    // Вычисляем дискретные коэффициенты
-    q0 = K * (1.0 + Td / T0);
-    q1 = -K * (1.0 + 2.0 * Td / T0 - T0 / T);
-    q2 = K * Td / T0;
+    // вспомогательная функция вычисления приращения
+    double computeDelta(
+        double k0, double k1, double k2,
+        double curErr,
+        double prevErr1,
+        double prevErr2)
+    {
+        return (k0 * curErr) +
+               (k1 * prevErr1) +
+               (k2 * prevErr2);
+    }
 }
 
-double PID::u_calc(double e) {
-    if (valid) {
-        double delta_u = q0 * e + q1 * e_prev_1 + q2 * e_prev_2;
-        double u = u_prev + delta_u;
+PID::PID(double gain, double ti, double td, double step)
+{
+    // сохраняем параметры
+    K  = gain;
+    T  = ti;
+    Td = td;
+    T0 = step;
 
-        u_prev = u;
-        e_prev_2 = e_prev_1;
-        e_prev_1 = e;
-        return u;
+    // вычисляем коэффициенты через вспомогательные выражения
+    const double invT0 = 1.0 / T0;
+
+    q0 = K * (1.0 + Td * invT0);
+    q1 = -K * (1.0 + 2.0 * Td * invT0 - (T0 / T));
+    q2 = K * Td * invT0;
+
+    valid = true;
+}
+
+double PID::u_calc(double error)
+{
+    if (!valid) {
+        std::cout << "Not valid value of 'q'!\n";
+        return 0.0;
     }
-    std::cout << "Not valid value of 'q'!\n";
-    return 0.0;
+
+    // вычисление приращения
+    const double du = computeDelta(
+        q0, q1, q2,
+        error,
+        e_prev_1,
+        e_prev_2
+    );
+
+    // обновляем управляющее воздействие
+    double current_u = u_prev + du;
+
+    // сдвигаем историю ошибок
+    e_prev_2 = e_prev_1;
+    e_prev_1 = error;
+
+    u_prev = current_u;
+
+    return current_u;
 }
