6515015ebcab5f1c7b20d2f156d27b6c42fc7f1c
Заремская Екатерина Павловна <zarekaterina22@gmail.com>
2025-12-04 22:17:28 +0300

    исправление ишью сонаркуба №1

--- trunk/ii02809/task_03/src/functions.cpp
+++ trunk/ii02809/task_03/src/functions.cpp
@@ -9,20 +9,24 @@ double nonlinear(double y, double u, double a, double b, double c, double d) {
     return a * y - b * pow(y, 2) + c * u + d * sin(u);
 }
 
-PID::PID(double K, double Ti, double Td, double To) {
-    q0 = K * (1 + Td / To);
-    q1 = -K * (1 + 2 * Td / To - To / Ti);
-    q2 = K * Td / To;
-    e_prev = 0.0;
-    e_prev2 = 0.0;
-    u_prev = 0.0;
+PID::PID(double K, double Ti, double Td, double To)
+    : q0(K * (1 + Td / To)),
+      q1(-K * (1 + 2 * Td / To - To / Ti)),
+      q2(K * Td / To),
+      e_prev(0.0),
+      e_prev2(0.0),
+      u_prev(0.0)
+{
+
 }
 
 double PID::compute(double e) {
     double delta_u = q0 * e + q1 * e_prev + q2 * e_prev2;
     double u = u_prev + delta_u;
+
     e_prev2 = e_prev;
-    e_prev = e;
-    u_prev = u;
+    e_prev  = e;
+    u_prev  = u;
+
     return u;
 }
