83aae018bf9ff6b8f64a23bfc3fd95cc388a78ad
Glouston <64912220+Glouston@users.noreply.github.com>
2025-12-05 10:46:36 +0200

    added const

--- trunk/ii02724/task_02/src/Nonlinear.h
+++ trunk/ii02724/task_02/src/Nonlinear.h
@@ -6,6 +6,6 @@ struct Constant {
 	double param_c;
 	double param_d;
 };
-void Nonlinear(Constant &c, int steps_count, double& current_temp, double current_heat, double next_temp, double previous_temp, double previous_heat);
+void Nonlinear(const Constant &c, int steps_count, double& current_temp, double current_heat, double next_temp, double previous_temp, double previous_heat);
 
 #endif 
