fcf72f2732ad563e70e670506a14d8b875b6e0ac
Федотов Дмитрий Александрович <fedotovd778@gmail.com>
2025-12-05 21:37:05 +0300

    const add

--- trunk/ii02721/task_02/src/n_linear.cpp
+++ trunk/ii02721/task_02/src/n_linear.cpp
@@ -5,6 +5,6 @@
 double linear(double A, double B, double temp_prev, double heat_now) {
 	return A * temp_prev + B * heat_now;
 }
-double nonlinear(Nonl& l) {
+double nonlinear(const Nonl& l) {
 	return  l.A * l.temp_now - l.B * std::pow(l.temp_prev, 2) + l.C * l.heat_now + l.D * std::sin(l.heat_prev);
 }
