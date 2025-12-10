35224069c403774b8cee8408f650793c014a26f5
PTUshnik <fiwer396@gmail.com>
2025-12-10 14:21:00 +0300

    update task_1

--- trunk/ii02824/task_01/src/task_01.cpp
+++ trunk/ii02824/task_01/src/task_01.cpp
@@ -1,52 +1,57 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <iomanip>
 
-double linear(double y, double u, double a, double b)
-{
+struct ModelParameters {
+    double a, b, c, d;
+};
+
+double computeLinear(double y, double u, double a, double b) {
     return a * y + b * u;
 }
 
-double non_linear(double y, double& y_p, double u, double a, double b, double c, double d)
-{
-    y_p = y;
-    return a * y - b * y_p * y_p + c * u + d * std::sin(u);
+double computeNonLinear(double y, double& prev_y, double u, const ModelParameters& params) {
+    double result = params.a * y - params.b * prev_y * prev_y +
+        params.c * u + params.d * std::sin(u);
+    prev_y = y;
+    return result;
 }
 
-int main()
-{
-    double y;
-    double y_p;
-    double u;
-    std::cout << "Enter Yn-1, Y and U values:\n";
-    std::cin >> y_p  >> y >> u;
-    
-    double a;
-    double b;
-    double c;
-    double d;
-    std::cout << "Enter A, B, C, D values:\n";
-    std::cin >> a >> b >> c >> d;
+void simulateModel(int steps, double u, const ModelParameters& params,
+    double init_y_linear, double init_y_nonlinear, double init_prev_y) {
 
-    int n;
-    std::cout << "Enter number of steps N: ";
-    std::cin >> n;
+    std::cout << "\nSimulation Results:\n";
+    std::cout << std::string(50, '-') << std::endl;
 
-    double y1 = y;
-    
-    double y2 = y;
-    double y2_p = y_p;
+    double y_linear = init_y_linear;
+    double y_nonlinear = init_y_nonlinear;
+    double prev_y_nonlinear = init_prev_y;
 
-    for (int i = 0; i < n; i++)
-    {
-        y1 = linear(y1, u, a, b);
-        std::cout << "The " << i << "st step of linear model - " << y1 << std::endl;
+    for (int step = 0; step < steps; ++step) {
+        y_linear = computeLinear(y_linear, u, params.a, params.b);
+        y_nonlinear = computeNonLinear(y_nonlinear, prev_y_nonlinear, u, params);
+
+        std::cout << "Step " << std::setw(2) << step << ": ";
+        std::cout << "Linear = " << std::setw(10) << std::fixed << std::setprecision(4) << y_linear;
+        std::cout << " | Non-linear = " << std::setw(10) << y_nonlinear << std::endl;
     }
+}
 
-    for (int i = 0; i < n; i++)
-    {
-        y2 = non_linear(y2, y2_p, u, a, b, c, d);
-        std::cout << "The " << i << "st step of nonlinear model - " << y2 << std::endl;
-    }
+int main() {
+    double y_current, y_previous, u_input;
+    std::cout << "Enter initial conditions (Y_{n-1}, Y_n, U): ";
+    std::cin >> y_previous >> y_current >> u_input;
+
+    ModelParameters params;
+    std::cout << "Enter model parameters (A, B, C, D): ";
+    std::cin >> params.a >> params.b >> params.c >> params.d;
+
+    int num_steps;
+    std::cout << "Enter number of simulation steps: ";
+    std::cin >> num_steps;
+
+    simulateModel(num_steps, u_input, params, y_current, y_current, y_previous);
 
     return 0;
 }
