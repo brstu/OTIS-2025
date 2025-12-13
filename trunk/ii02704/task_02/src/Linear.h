0b751133723759efe5dc021f786fb234c4bcff4f
Гридчин Александр <gridchin228@gmail.com>
2025-12-13 21:57:12 +0200

    Update Linear.h

--- trunk/ii02704/task_02/src/Linear.h
+++ trunk/ii02704/task_02/src/Linear.h
@@ -5,6 +5,6 @@
 #include <vector>
 
 void input(int& k, std::vector<double>& u, std::vector<double>& y, double& a, double& b);
-void linear(const int& place, const int& k, std::vector<double>& y, const std::vector<double>& u, const double& b, const double& a);
+void linear(const int& place, const int& k, std::vector<double>& y, const std::vector<double>& u, const double& a, const double& b);
 
 #endif
