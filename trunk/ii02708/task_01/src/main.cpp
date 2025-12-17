ceb6d600ac75af138e46035e96a32f2ad71427a0
kriskess <kirverand@gmail.com>
2025-12-17 14:57:41 +0300

    update

--- trunk/ii02708/task_01/src/main.cpp
+++ trunk/ii02708/task_01/src/main.cpp
@@ -1,39 +1,39 @@
 #include<iostream>
 #include<cmath>
-using namespace std;
 
-float a=0.5,
-	b = 0.2,
-	c= 0.15,
-	d = 0.3,
-	u = 1.2;
 int main()
 {
+	const float a = 0.5f;
+	const float b = 0.2f;
+	const float c = 0.15f;
+	const float d = 0.3f;
+	const float u = 1.2f;
+
 	float y0;
 	float y1;
 	float y2;
 	float y_temp;
-	cout << "y1 = ";
-	cin >> y_temp;
+	std::cout << "y1 = ";
+	std::cin >> y_temp;
 	y1 = y_temp;
 	y0 = y1;
 	int n = 10;
-	cout << endl << "Linear"<<endl;
+	std::cout << std::endl << "Linear"<<std::endl;
 	for (int i = 0; i < n; i++)
 	{
-		cout << "t" << i + 1 << " = ";
+		std::cout << "t" << i + 1 << " = ";
 		y2 = a * y1 + b * u;
-		cout << y2 << endl;
+		std::cout << y2 << std::endl;
 		y0 = y1;
 		y1 = y2;
 	}
 
-	cout << endl << "Nonlinear:" << endl;
+	std::cout << std::endl << "Nonlinear:" << std::endl;
 	for (int i = 0; i < n; i++)
 	{
-		cout << "t" << i + 1 << " = ";
+		std::cout << "t" << i + 1 << " = ";
 		y2 = a * y1 - b * y0 * y0 + c * u + d * sin(u);
-		cout << y2 << endl;
+		std::cout << y2 << std::endl;
 		y0 = y1;
 		y1 = y2;
 	}
