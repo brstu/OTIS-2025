08248b8444e4a371dc59239ff7fb7ac9f9e401ea
Ilya Oleksyuk <brest155304@gmail.com>
2025-12-17 21:52:03 +0300

    123

--- trunk/ii02822/task_03/src/model.cpp
+++ trunk/ii02822/task_03/src/model.cpp
@@ -2,57 +2,47 @@
 
 class ModelState {
 private:
-    static double& getK() {
-        inline static double K_obj = 1.0;
-        return K_obj;
-    }
-
-    static double& getT() {
-        inline static double T_obj = 0.5;
-        return T_obj;
-    }
-
-    static double& getYPrev() {
-        inline static double y_prev = 0.0;
-        return y_prev;
-    }
+    inline static double K_obj = 1.0;
+    inline static double T_obj = 0.5;
+    inline static double y_prev = 0.0;
 
 public:
     static void init(double K, double T) {
-        getK() = K;
-        getT() = T;
-        getYPrev() = 0.0;
+        K_obj = K;
+        T_obj = T;
+        y_prev = 0.0;
     }
 
     static double update(double u, double dt) {
         double a = 0.0;
         double b = 0.0;
 
-        if (getT() > 0.0) {
-            a = 1.0 - dt / getT();
-            b = getK() * dt / getT();
+        if (T_obj > 0.0) {
+            a = 1.0 - dt / T_obj;
+            b = K_obj * dt / T_obj;
         }
         else {
             a = 0.0;
-            b = getK();
+            b = K_obj;
         }
 
-        const double y = a * getYPrev() + b * u;
-        getYPrev() = y;
+        const double y = a * y_prev + b * u;
+        y_prev = y;
 
         return y;
     }
 
     static void reset() {
-        getYPrev() = 0.0;
+        y_prev = 0.0;
     }
 
     static void setParameters(double K, double T) {
-        getK() = K;
-        getT() = T;
+        K_obj = K;
+        T_obj = T;
         reset();
     }
 };
+
 
 void modelInit(double K, double T) {
     ModelState::init(K, T);
