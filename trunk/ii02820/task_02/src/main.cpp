616e8aac1906b04cbdaa15016dbb58a697b36bdc
anton <anton.malich007@gmail.com>
2025-12-17 19:02:21 +0300

    new update

--- trunk/ii02820/task_02/src/main.cpp
+++ trunk/ii02820/task_02/src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <vector>  
+#include <cmath>
 #include "model.h"
-
+#include <vector>
 using namespace std;
 
 int main() {
@@ -10,26 +10,36 @@ int main() {
     double c = 0.05;
     double d = 0.1;
     double initial_y = 23;
-    
-    LinearModel linear_model(a, b, initial_y);
-    NonlinearModel nonlinear_model(a, b, c, d, initial_y);
-    
-   
-    std::vector<double> u = { 10, 15, 20, 25, 30, 25, 20, 15, 10, 5 };
-    
-    
-    const size_t steps = u.size();
-    
-    cout << "Model :" << endl;
+
+    double u[] = { 10, 15, 20, 25, 30, 25, 20, 15, 10, 5 };
+    int steps = 10;
+
+    cout << "Model Comparison:" << endl;
     cout << "Time\tu\tLinear\tNonlinear" << endl;
+    
+    double y_linear = initial_y;
+    double y_nonlinear_prev = initial_y;
+    double y_nonlinear_current = initial_y;
+    
     cout << "0\t0\t" << initial_y << "\t" << initial_y << endl;
-    
-    for (size_t i = 0; i < steps; i++) {
+
+    for (int i = 0; i < steps; i++) {
         double power = u[i];
-        double y_linear = linear_model.calculate(power);
-        double y_nonlinear = nonlinear_model.calculate(power);
-        cout << i + 1 << "\t" << power << "\t" << y_linear << "\t" << y_nonlinear << endl;
+        
+        y_linear = linear(y_linear, power, a, b);
+        double new_y_nonlinear = non_linear(
+            y_nonlinear_current, 
+            y_nonlinear_prev, 
+            power, 
+            a, b, c, d
+        );
+        
+        y_nonlinear_prev = y_nonlinear_current;
+        y_nonlinear_current = new_y_nonlinear;
+        
+        cout << i + 1 << "\t" << power << "\t" 
+             << y_linear << "\t" << y_nonlinear_current << endl;
     }
-    
+
     return 0;
 }
