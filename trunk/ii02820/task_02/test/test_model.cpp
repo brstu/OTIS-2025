6e063d7132fe5ad055d5018d476c094c70e120ae
anton <anton.malich007@gmail.com>
2025-12-17 20:04:38 +0300

    лл

--- trunk/ii02820/task_02/test/test_model.cpp
+++ trunk/ii02820/task_02/test/test_model.cpp
@@ -1,32 +1,14 @@
 #include <gtest/gtest.h>
 #include "model.h"
 
-TEST(LinearModelTest, BasicCalculation) {
-    // y = 0.95*23 + 0.002*10 = 21.85 + 0.02 = 21.87
-    EXPECT_NEAR(linear_model(23.0, 10.0, 0.95, 0.002), 21.87, 0.01);
+TEST(ModelTest, LinearModel) {
+    double result = linear_model(23.0, 10.0, 0.95, 0.002);
+    EXPECT_NEAR(result, 21.87, 0.01);
 }
 
-TEST(LinearModelTest, ZeroInput) {
-    EXPECT_DOUBLE_EQ(linear_model(10.0, 0.0, 0.5, 0.1), 5.0);
-}
-
-TEST(LinearModelTest, ZeroParameters) {
-    EXPECT_DOUBLE_EQ(linear_model(10.0, 5.0, 0.0, 0.0), 0.0);
-}
-
-TEST(NonlinearModelTest, BasicCalculation) {
-    // y = 0.95*23 + 0.05*22 + 0.05*10 + 0.0001*100
-    //   = 21.85 + 1.1 + 0.5 + 0.01 = 23.46
+TEST(ModelTest, NonlinearModel) {
     double result = nonlinear_model(23.0, 22.0, 10.0, 0.95, 0.05, 0.05, 0.0001);
     EXPECT_NEAR(result, 23.46, 0.01);
-}
-
-TEST(NonlinearModelTest, AllZeros) {
-    EXPECT_DOUBLE_EQ(nonlinear_model(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0), 0.0);
-}
-
-TEST(NonlinearModelTest, OnlyInput) {
-    EXPECT_DOUBLE_EQ(nonlinear_model(0.0, 0.0, 10.0, 0.0, 0.0, 1.0, 0.0), 10.0);
 }
 
 int main(int argc, char **argv) {
