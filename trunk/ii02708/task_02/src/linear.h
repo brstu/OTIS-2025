44d03bb721e3b1aa8eb7692b55ad1883e7a832c9
kriskess <kirverand@gmail.com>
2025-12-17 17:18:50 +0300

    Initial commit for lab2.

---
+++ trunk/ii02708/task_02/src/linear.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+
+struct LinearCoeff{
+    double a;
+    double b;
+    double u;
+};
+
+std::vector<double> calculateLinear(LinearCoeff coeff, double y0, int n)
+{
+    std::vector<double> results;
+    double y1 = y0;
+    double y2 = y0;
+    for (int i = 0; i < n; i++)
+	{
+		y2 = coeff.a * y1 + coeff.b * coeff.u;
+        results.push_back(y2);
+		y0 = y1;
+		y1 = y2;
+	}
+    return results;
+}
