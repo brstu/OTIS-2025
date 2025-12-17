48c62072254012f638440636f62d411f1c3d74bc
Boss <ii002714@g.bstu.by>
2025-12-18 01:20:57 +0300

    changed language of explanation

--- trunk/ii02714/task_03/src/PID.h
+++ trunk/ii02714/task_03/src/PID.h
@@ -1,12 +1,11 @@
-#ifndef PID_H
+п»ї#ifndef PID_H
 #define PID_H 
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 /**
- * \brief Класс для иметации PID регулятора
- * 
- */
+* \brief Class for implementing a PID controller
+*/
 class PID {
 private:
 	double q0;
@@ -25,51 +24,50 @@ private:
 	double a;
 	double b;
 public:
-	/**
-	* \brief Конструктор для инцеализации переменных и векторов
-	*/
+/**
+* \brief Constructor for instantiating variables and vectors
+*/
 	PID();
-	/**
- * \brief Линейная функции имеющая вид: y[t] = a * y[t - 1] + b * u[t - 1]
- * \param place служит для определения температуры в определнный момент
- */
+/**
+* \brief A linear function of the form: y[t] = a * y[t - 1] + b * u[t - 1]
+* \param place is used to determine the temperature at a certain moment
+*/
 	void linear(int place);
-	/**
-  * \brief Функция для вычисления delta_u[place] delta_u переменная необходимая для вычисления U
-  * \param place служит для определения температуры в определнный момент
-  */
+/**
+*\brief Function for calculating delta_u[place] delta_u variable needed to calculate U
+* \param place is used to determine the temperature at a specific moment
+*/
 	void calcul_DeltU(int place);
-	/**
-* \brief Функция вычисления U
-*  \param place служит для определения температуры в определнный момент
+/**
+* \brief Р¤СѓРЅРєС†РёСЏ РІС‹С‡РёСЃР»РµРЅРёСЏ U
+*  \param place СЃР»СѓР¶РёС‚ РґР»СЏ РѕРїСЂРµРґРµР»РµРЅРёСЏ С‚РµРјРїРµСЂР°С‚СѓСЂС‹ РІ РѕРїСЂРµРґРµР»РЅРЅС‹Р№ РјРѕРјРµРЅС‚
 */
 	void calcul_U(int place);
-	/**
-* \brief Функция вычисления q0 q1 q2
-* Переменные k, Td, To, T являются константами необходимыми для вычисления значений
+/**
+* \brief U calculation function
+* \param place is used to determine the temperature at a specific moment
 */
 	void calcul_q0_q1_q2();
-	/**
-* \brief Функция вычисления ошибку e
+/**
+* \brief Calculate error function e
 */
 	void calcul_e(int place);
-	/**
-* \brief Основная функция иметирующая работу PID регулятора
-*  \param place служит для определения температуры в определнный момент
+/**
+* \brief The main function that simulates the operation of the PID controller
+* \param place is used to determine the temperature at a specific moment
 */
 	void calcul_PID(int place);
-	/**
-* \brief Функция регулирующая значения констант а и b уменьшая их
-* \param val значение на которое делится а
-*  b делиться на 1.3
+/**
+* \brief A function that adjusts the values вЂ‹вЂ‹of constants a and b, decreasing them.
+* \param val is the value by which a is divided.
 */
 	void RegCoefs(double val);
-	/**
-* \brief Функция для получения значений q0 q1 q2
-* Вызывается в тестах для проверки работы программы
-* \param val1 присваивает значение q0
-* \param val2 присваивает значение q1
-* \param val3 присваивает значение q2
+/**
+* \brief Function for getting the values вЂ‹вЂ‹of q0, q1, q2
+* Called in tests to check the program's operation
+* \param val1 assigns the value to q0
+* \param val2 assigns the value to q1
+* \param val3 assigns the value to q2
 */
 	void GetQs(double &val1,double &val2,double &val3) const;
 };
