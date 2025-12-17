5c8b3f35062a8effce63320c161002fa7801bf51
anton <anton.malich007@gmail.com>
2025-12-17 20:25:10 +0300

    u

--- trunk/ii02820/task_02/test/test_model.cpp
+++ trunk/ii02820/task_02/test/test_model.cpp
@@ -1,14 +1,36 @@
+#define _USE_MATH_DEFINES
 #include <gtest/gtest.h>
-#include "model.h"
+#include <cmath>
+#include "../src/model.h"
 
-TEST(ModelTest, LinearModel) {
-    double result = linear_model(23.0, 10.0, 0.95, 0.002);
-    EXPECT_NEAR(result, 21.87, 0.01);
+TEST(LinearModelTest, PositiveValues) {
+    EXPECT_DOUBLE_EQ(linear_model(2.0, 3.0, 1.0, 4.0), 2.0 * 1.0 + 3.0 * 4.0);
 }
 
-TEST(ModelTest, NonlinearModel) {
-    double result = nonlinear_model(23.0, 22.0, 10.0, 0.95, 0.05, 0.05, 0.0001);
-    EXPECT_NEAR(result, 23.46, 0.01);
+TEST(LinearModelTest, ZeroInput) {
+    EXPECT_DOUBLE_EQ(linear_model(1.5, 2.0, 0.0, 0.0), 0.0);
+}
+
+TEST(LinearModelTest, NegativeValues) {
+    EXPECT_DOUBLE_EQ(linear_model(-1.0, 2.0, 3.0, -4.0), -1.0 * 3.0 + 2.0 * -4.0);
+}
+
+TEST(NonLinearModelTest, BasicCalculation) {
+    double res = nonlinear_model(1.0, 1.0, 1.0, 0.0, 2.0, 3.0, 1.0, 0.0);
+    double expected = 1.0 * 3.0 - 1.0 * pow(2.0, 2) + 1.0 * 1.0 + 0.0 * sin(0.0);
+    EXPECT_NEAR(res, expected, 1e-9);
+}
+
+TEST(NonLinearModelTest, IncludesSineTerm) {
+    double res1 = nonlinear_model(1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, M_PI / 2);
+    double expected = 1.0 * 1.0 - 0.0 * 0.0 + 0.0 * 0.0 + 1.0 * sin(M_PI / 2);
+    EXPECT_NEAR(res1, expected, 1e-9);
+}
+
+TEST(NonLinearModelTest, NegativeInput) {
+    double res = nonlinear_model(-1.0, 1.0, 2.0, -1.0, 1.0, -2.0, 3.0, M_PI);
+    double expected = -1.0 * -2.0 - 1.0 * 1.0 + 2.0 * 3.0 - 1.0 * sin(M_PI);
+    EXPECT_NEAR(res, expected, 1e-9);
 }
 
 int main(int argc, char **argv) {
