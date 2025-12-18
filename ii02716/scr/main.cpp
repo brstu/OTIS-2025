92a42c934a6c48d41dae2cab91e339ded144d866
Ничипорчик Дарья <daryannn1@icloud.com>
2025-12-18 10:48:45 +0300

    Update main.cpp

--- ii02716/scr/main.cpp
+++ ii02716/scr/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <limits>  // Добавлено для clear()
 
 using namespace std;
 
@@ -16,12 +17,12 @@ struct SystemConstants {
 class LinearSystem {
 private:
     float a, b, u;
-    
+
 public:
     LinearSystem(float a_val, float b_val, float u_val) 
         : a(a_val), b(b_val), u(u_val) {}
-    
-    float calculateNext(float y1) {
+
+    float calculateNext(float y1) const {  // Добавлен const
         return a * y1 + b * u;
     }
 };
@@ -30,54 +31,54 @@ public:
 class NonlinearSystem {
 private:
     float a, b, c, d, u;
-    
+
 public:
     NonlinearSystem(float a_val, float b_val, float c_val, 
                    float d_val, float u_val) 
         : a(a_val), b(b_val), c(c_val), d(d_val), u(u_val) {}
-    
-    float calculateNext(float y1, float y0) {
+
+    float calculateNext(float y1, float y0) const {  // Добавлен const
         return a * y1 - b * y0 * y0 + c * u + d * sin(u);
     }
 };
 
 // Функция для вывода результатов
-void printResult(const string& title, int iteration, float value) {
-    cout << title << " t" << iteration + 1 << " = " << value << endl;
+void printResult(int iteration, float value) {  // Убран неиспользуемый параметр
+    cout << " t" << iteration + 1 << " = " << value << endl;
 }
 
 // Основная функция симуляции
 void simulateSystem(float initialY1, int iterations) {
     SystemConstants constants;
-    
+
     // Инициализация систем
     LinearSystem linearSys(constants.a, constants.b, constants.u);
     NonlinearSystem nonlinearSys(constants.a, constants.b, 
                                 constants.c, constants.d, constants.u);
-    
+
     // Переменные для состояний
     float y0 = initialY1;
     float y1 = initialY1;
     float y2;
-    
+
     // Линейная система
     cout << "\nLinear system simulation:" << endl;
     for (int i = 0; i < iterations; i++) {
         y2 = linearSys.calculateNext(y1);
-        printResult("", i, y2);
+        printResult(i, y2);
         y0 = y1;
         y1 = y2;
     }
-    
+
     // Сброс состояний для нелинейной системы
     y0 = initialY1;
     y1 = initialY1;
-    
+
     // Нелинейная система
     cout << "\nNonlinear system simulation:" << endl;
     for (int i = 0; i < iterations; i++) {
         y2 = nonlinearSys.calculateNext(y1, y0);
-        printResult("", i, y2);
+        printResult(i, y2);
         y0 = y1;
         y1 = y2;
     }
@@ -86,13 +87,19 @@ void simulateSystem(float initialY1, int iterations) {
 int main() {
     const int ITERATIONS = 10;
     float initialY1;
-    
-    // Ввод начального условия
+
+    // Ввод начального условия с проверкой
     cout << "Enter initial value y1 = ";
-    cin >> initialY1;
     
+    // Проверка корректности ввода
+    while (!(cin >> initialY1)) {
+        cout << "Invalid input. Please enter a number: ";
+        cin.clear();  // Сброс флага ошибки
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Очистка буфера
+    }
+
     // Запуск симуляции
     simulateSystem(initialY1, ITERATIONS);
-    
+
     return 0;
 }
