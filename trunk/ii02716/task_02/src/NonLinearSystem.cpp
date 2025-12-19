32485726cd9df6e044053daab9bfb5e651275187
Ничипорчик Дарья <daryannn1@icloud.com>
2025-12-19 23:27:11 +0300

    Update NonLinearSystem.cpp

--- trunk/ii02716/task_02/src/NonLinearSystem.cpp
+++ trunk/ii02716/task_02/src/NonLinearSystem.cpp
@@ -1,6 +1,6 @@
 #include "NonLinearSystem.h"
 
-float NonlinearSystem::calculateNext(float y1, float y0) const {  
+double NonlinearSystem::calculateNext(float y1, float y0) const {  
 
     return a * y1 - b * y0 * y0 + c * u + d * sin(u);
 
