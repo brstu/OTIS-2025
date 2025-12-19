3e7c6dd6c3068502f059e1055b58feb40941a0d1
whytr1ble <tr1bleeeee@gmail.com>
2025-12-19 20:13:29 +0300

    task_02 done
    Lab 02 done

---
+++ trunk/ii02703/task_02/src/models.cpp
@@ -0,0 +1,10 @@
+﻿#include "models.hpp"
+#include <cmath>
+
+double linear_model(double y, double u, double a, double b) {
+    return a * y + b * u;
+}
+
+double nonlinear_model(double y_curr, double y_prev, double u, double u_prev, double a, double b, double c, double d) {
+    return a * y_curr - b * (y_prev * y_prev) + c * u + d * std::sin(u_prev);
+}
