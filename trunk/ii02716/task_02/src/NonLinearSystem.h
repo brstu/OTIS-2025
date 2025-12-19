a5588b45433a7c7d30e958822ca25619c260c8c6
Ничипорчик Дарья <daryannn1@icloud.com>
2025-12-19 23:26:59 +0300

    Update NonLinearSystem.h

--- trunk/ii02716/task_02/src/NonLinearSystem.h
+++ trunk/ii02716/task_02/src/NonLinearSystem.h
@@ -16,7 +16,7 @@ public:
         : a(a_val), b(b_val), c(c_val), d(d_val), u(u_val) {
     }
 
-    float calculateNext(float y1, float y0) const;
+    double calculateNext(float y1, float y0) const;
 };
 
 
