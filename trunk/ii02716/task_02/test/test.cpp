1333cfeaad5ef47c98717aff48941031f7a32d68
Boss <ii002715@g.bstu.by>
2025-12-19 22:43:35 +0300

    files

---
+++ trunk/ii02716/task_02/test/test.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "../src/"
+
+TEST(TestFuns, linear) {
+	double y_next;
+	double res;
+	res = Linear(2.3, 3.2, 7.8, y_next, 7.8);
+	EXPECT_NEAR(res, 42.9, 0.1);
+}
+TEST(TestFuns, linear2) {
+	double y_next;
+	double res;
+	res = Linear(4.3, 3.2, 5.8, y_next, 2.8);
+	EXPECT_NEAR(res, 33.9, 0.1);
+}
+TEST(TestFuns, nonlinear) {
+	double y_next;
+	double res;
+	res = Nonlinear(2.3, 4.3, 1.5, 6.2, 1.6, 5.7, 7.2, y_next, 1.1);
+	EXPECT_NEAR(res, 4.9, 0.1);
+}
+TEST(TestFuns, nonlinear2) {
+	double y_next;
+	double res;
+	res = Nonlinear(3.3, 2.3, 2.5, 1.2, 5.6, 2.7, 0.2, y_next, 3.7);
+	EXPECT_NEAR(res, -52.9, 0.1);
+}
