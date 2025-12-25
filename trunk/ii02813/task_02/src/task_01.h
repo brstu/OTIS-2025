7c1c17933e15e31a1b1352b834484f91e8e1c147
Ковалевич Евгений <ekovze@gmail.com>
2025-12-25 19:52:17 +0300

    fix2

--- trunk/ii02813/task_02/src/task_01.h
+++ trunk/ii02813/task_02/src/task_01.h
@@ -1,4 +1,4 @@
 #pragma once
 
-double linear(double y01, double u01, double a01, double b01);
+double linear(double u01, double y01 , double a01, double b01);
 double non_linear(double y01, double& y_p01, double u01, double a01, double b01, double c01, double d01);
