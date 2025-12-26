c426b8bd7a11c6af1a99ec3c11c110c04bd46332
Aleksey <alexei.tsitunin@gmail.com>
2025-12-26 14:59:13 +0300

    Initial commit

---
+++ trunk/ii02715/task_03/doc/html/functions_8h.js
@@ -0,0 +1,4 @@
+var functions_8h =
+[
+    [ "linear", "functions_8h.html#a6b45a75bd62337f0dd51686476b27a8d", null ]
+];
---
+++ trunk/ii02715/task_03/src/functions.cpp
@@ -0,0 +1,30 @@
+#include "functions.h"
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+void linear(double a, double b, std::vector<double>& y, const int steps, double target)
+{
+	double u;
+	double P = 1.6;
+	double I = 0.32;
+	double D = 0.4;
+	double e = 0;
+	double e0;
+	double sumE = 0;
+	double dE;
+	double er = 0;
+
+	for (int i = 1; i < steps; i++)
+	{
+		e0 = e;
+		e = target - y[i - 1];
+		sumE += e;
+		dE = e - e0;
+		u = P * e + I * sumE + D * dE;
+		y[i] = a * y[i - 1] + b * u;
+		std::cout << "y[" << i << "] = " << y[i] << std::endl;
+		er = abs(y[i] - target);
+		std::cout << "Error of value: " << er << std::endl << std::endl;
+	}
+}
